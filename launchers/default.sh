fa9b6165e926fbba76f243fd527665a92557e0e2
JesseCoenraad <150351522+JesseCoenraad@users.noreply.github.com>
2026-06-22 14:51:55 +0200

    Update default.sh

--- launchers/default.sh
+++ launchers/default.sh
@@ -12,8 +12,13 @@ dt-launchfile-init
 # NOTE: Use the variable DT_REPO_PATH to know the absolute path to your code
 # NOTE: Use `dt-exec COMMAND` to run the main process (blocking process)
 
-# launching app
-dt-exec echo "This is an empty launch script. Update it to launch your application."
+# start/goal node can be overridden via env vars, e.g. for a different test map
+START_NODE="${START_NODE:-S}"
+GOAL_NODE="${GOAL_NODE:-T}"
+
+# launching app: path planning (task 1) + localization (task 2) + navigation (task 3)
+dt-exec roslaunch --wait navigation full_system.launch \
+  start:="${START_NODE}" goal:="${GOAL_NODE}"
 
 
 # ----------------------------------------------------------------------------
